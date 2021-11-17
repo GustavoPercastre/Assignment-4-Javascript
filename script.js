0294edb6693593468f6707daee9f7ee68903f177
GustavoPercastre <angelpercastre293@gmail.com>
2021-11-16 23:39:31 -0800

    Created the function that takes the radius of a circle and outputs it’s approximate circumference.

--- script.js
+++ script.js
@@ -3,15 +3,18 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
-console.log(Math.min(25, 138));
+console.log(Math.min(0, 10));
 
-console.log(Math.min(100, -10));
+console.log(Math.min(0, -10));
 
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
 
-
+function calculateCircumference(radius) 
+{
+    return 2 * Math.PI * radius;
+}
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
